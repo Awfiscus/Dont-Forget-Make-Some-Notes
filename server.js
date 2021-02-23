9fc78541a5119cd2234cee4d278c5cddf035221a
Drew Weston <awfiscus@comcast.net>
2021-02-22 19:00:09 -0700

    cleaned up code

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const bd = require("./db/db.json");
+
+//npm used to give each new note an unique id
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
@@ -10,7 +12,6 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const noteTitles = [];
 let notes = [];
 
 app.use(express.static(__dirname + "/public"));
@@ -27,6 +28,7 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
+//takes whatever is in the json file then save it to the notes array
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
@@ -37,6 +39,7 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
+//adds new note from front end to notes array then writes it to the json file
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
 
@@ -49,8 +52,7 @@ app.post("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-//STOPPED HERE
-
+//deletes note from json file based on id sent from front end
 app.delete("/api/notes/:id", (req, res) => {
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     let id = req.params.id;
